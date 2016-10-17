3597cf3e8d46cfa4f52a9b0e635a91f766f091ef
Ubuntu <ubuntu@ip-172-31-18-54.us-west-2.compute.internal>
2016-10-17 00:47:01 +0000

    Add feature sloc count

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,7 @@ import os
 import collections
 from post_comment import post_comment
 from post_commit_comment import post_commit_comment
-
+from sloc_count import sloc_count
 from flask import Flask, render_template, request, url_for,jsonify
 from flask_json_multidict import get_json_multidict
 from github import Github
@@ -17,8 +17,14 @@ COMMENT_ON_PR="pull"
 COMMENT_ON_ISSUE="issues"
 CODE_KAKA="codekaka"
 PR="pull_request"
+
+#comment_type because, GitHub doesn't differentiate the comments between issues comments and PR comments.
 PR_COMMENT="issue_comment"
 COMMIT_COMMENT="commit_comment"
+
+PR_SIZE_SMALL=5
+PR_SIZE_MEDIUM=10
+#PR_SIZE_LARGE=2
 
 class crabot:
     def __init__(self, user, repo, method,request):
@@ -42,6 +48,8 @@ class crabot:
             #Extracting payload and converting it from unicode to str
             str_req_dict=str(request_dict['payload'])
             temp_dict=str_req_dict.replace("'", "\"")
+
+            #loading a json object from the string
             dict_payload=json.loads(temp_dict)
             return dict_payload
 
@@ -66,31 +74,54 @@ class crabot:
 
     #Process tagged comment
     def process_tagged_comment(self,comment):
+            reply="You have selected option"
+            if comment.find(1):
+                reply+="1,"
+            elif comment.find(2):
+                reply+="2,"
             return "You just tagged me! I am always at your service"
 
-    def get_reply_to_PR(self):
-            return "Pull request was made succesfully, do you want me to run the below analysis? 1. Code coverage, 2. New dependencies added, 3. Documentation"
+    def get_reply_to_PR(self,dict_payload):
+            return "Here are the results of your PR"
 
     def get_reply_to_CC(self):
         return "Responding to commit comment"
 
-    def respond_to_comment(self,dict_payload,rest_git):
+    def respond_to_PR_comment(self,dict_payload,rest_git):
             comment,issue_num,commenting_user,comment_type=self.get_comment_details(dict_payload)
             if(not (commenting_user == 'codekaka')  and comment_type is COMMENT_ON_PR and self.is_codekaka_tagged(comment)):
                     reply=self.process_tagged_comment(comment)
-                    print "before psot comment"
+                    print "before post comment"
                     post_comment_status=post_comment(reply, rest_git, self.user,self.repo,issue_num)
                     print "SUCCESS" if isinstance(post_comment_status, IssueComment.IssueComment) else "FAILURE"
 
-    def repsond_to_PR(self,rest_git,issue_num):
+    def repsond_to_PR(self,rest_git,dict_payload):
             reply=self.get_reply_to_PR()
-            post_comment_status=post_comment(reply, rest_git,self.user,self.repo,issue_num)
-            print "SUCCESS" if isinstance(post_comment_status, IssueComment.IssueComment) else "FAILURE"
+            issue_num=dict_payload['pull_request']['number']
+            pr_diff_url=dict_payload['pull_request']['diff_url']
+            pr_size=sloc_count(pr_diff_url)
+            if(pr_size<=PR_SIZE_SMALL):
+                return
+            elif(pr_size<=PR_SIZE_MEDIUM):
+                reply=get_reply_to_PR(dict_payload)
+                post_comment_status=post_comment(reply, rest_git,self.user,self.repo,issue_num)
+                print "SUCCESS" if isinstance(post_comment_status, IssueComment.IssueComment) else "FAILURE"
+            elif(pr_size>PR_SIZE_MEDIUM):
+                reply="Pull request was made succesfully, do you want me to run the below analysis? 1. Code coverage, 2. New dependencies added, 3. Documentation"
+                post_comment_status=post_comment(reply, rest_git,self.user,self.repo,issue_num)
+                print "SUCCESS" if isinstance(post_comment_status, IssueComment.IssueComment) else "FAILURE"
 
-    def respond_to_commit_comment(self,rest_git,sha_num):
+
+
+    def respond_to_commit_comment(self,rest_git,dict_payload):
         reply=self.get_reply_to_CC()
-        post_comment_status=post_commit_comment(reply, rest_git,self.user,self.repo,sha_num)
-        print "SUCCESS" if isinstance(post_comment_status, CommitComment.CommitComment) else "FAILURE"
+        sha_num=dict_payload['comment']['commit_id']
+        comment=dict_payload['comment']['body']
+        if(self.is_codekaka_tagged(comment)):
+            post_comment_status=post_commit_comment(reply, rest_git,self.user,self.repo,sha_num)
+            print "SUCCESS" if isinstance(post_comment_status, CommitComment.CommitComment) else "FAILURE"
+
+
 
     def process_request(self):
         #Extracting the information from the request object
@@ -109,15 +140,14 @@ class crabot:
 
         if(self.method==PR_COMMENT):
                 print "inside PR_COMMENT"
-                self.respond_to_comment(dict_payload,rest_git)
+                self.respond_to_PR_comment(dict_payload,rest_git)
         elif(self.method==PR):
                 print "inside PR"
-                issue_num=dict_payload['pull_request']['number']
-                self.repsond_to_PR(rest_git,issue_num)
+                self.repsond_to_PR(rest_git,dict_payload)
         elif(self.method==COMMIT_COMMENT):
             print "inside commit comment"
-            sha_num=dict_payload['comment']['commit_id']
-            self.respond_to_commit_comment(rest_git,sha_num)
+
+            self.respond_to_commit_comment(rest_git,dict_payload)
 
 
 #Main function that handles the post request
