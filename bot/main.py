7b7f48030b80b2af354d2bfa62bb5dbba7e365cb
Chethan <cnanniv@ncsu.edu>
2016-11-03 22:52:45 +0000

    added util dependency code, patch parsing is still pending

--- bot/main.py
+++ bot/main.py
@@ -34,7 +34,7 @@ DESCRIPTION_TEXT="""Do you want me to run any or all of the following analysis?
                     Example 2: To run all the analysis, reply with "run all".
                     Note: Reply commands are not case sensitive."""
 
-#comment_type because, GitHub doesn't differentiate the comments between issues comments and PR comments.
+#comment_type because, GitHub doesn't differentiate the comments between issues comments and PR comments. Issue comments are ignored
 PR_COMMENT="issue_comment"
 COMMIT_COMMENT="commit_comment"
 
@@ -64,7 +64,7 @@ class crabot:
             #Extracting payload and converting it from unicode to str
             str_req_dict=str(request_dict['payload'])
             temp_dict=str_req_dict.replace("'", "\"")
-	    temp_dict=str(str_req_dict)
+            temp_dict=str(str_req_dict)
             #loading a json object from the string
             dict_payload=json.loads(temp_dict)
             return dict_payload
@@ -89,7 +89,7 @@ class crabot:
             return comment.find(CODE_KAKA)> -1
 
     #Process tagged comment
-    def process_tagged_comment(self,comment,dict_payload):
+    def process_tagged_comment(self,comment,dict_payload,request_type):
             print "comment is "+ comment
             reply=""
             results=""
@@ -98,7 +98,7 @@ class crabot:
             if comment.find('run all')>-1:
                 reply="Ran all the analysis and here are the results"
                 results=util_coverage_calc()
-                results+=util_dependency_checker(dict_payload)
+                results+=util_dependency_checker(dict_payload,request_type)
                 results+=util_duplicates_checker()
                 results+=util_docu_collector()
                 return reply,results
@@ -109,7 +109,7 @@ class crabot:
             if comment.find('s2')>-1:
                 count+=1
                 reply+=", s2"
-                results+=util_dependency_checker(dict_payload)
+                results+=util_dependency_checker(dict_payload,request_type)
             if comment.find('s3')>-1:
                 count+=1
                 reply+=", s3"
@@ -132,7 +132,7 @@ class crabot:
     def respond_to_PR_comment(self,dict_payload,rest_git):
             comment,issue_num,commenting_user,comment_type=self.get_comment_details(dict_payload)
             if(not (commenting_user == 'codekaka')  and comment_type is COMMENT_ON_PR and self.is_codekaka_tagged(comment)):
-                    reply,results=self.process_tagged_comment(comment)
+                    reply,results=self.process_tagged_comment(comment,dict_payload,COMMENT_ON_PR)
                     print "before post comment"
                     post_comment_status=post_comment(reply+results, rest_git, self.user,self.repo,issue_num)
                     print "SUCCESS" if isinstance(post_comment_status, IssueComment.IssueComment) else "FAILURE"
@@ -151,7 +151,7 @@ class crabot:
             elif(pr_size<=PR_SIZE_MEDIUM):
                 re=""
                 reply+=" This is a medium sized Pull Request."
-                re,result=self.process_tagged_comment('@codekaka run all',dict_payload)
+                re,result=self.process_tagged_comment('@codekaka run all',dict_payload,PR)
                 reply+=result
 
             elif(pr_size>PR_SIZE_MEDIUM):
@@ -165,7 +165,7 @@ class crabot:
         sha_num=dict_payload['comment']['commit_id']
         comment=dict_payload['comment']['body']
         if(self.is_codekaka_tagged(comment)):
-            reply,results=self.process_tagged_comment(comment,dict_payload)
+            reply,results=self.process_tagged_comment(comment,dict_payload,COMMIT_COMMENT)
             post_comment_status=post_commit_comment(reply+results, rest_git,self.user,self.repo,sha_num)
             print "SUCCESS" if isinstance(post_comment_status, CommitComment.CommitComment) else "FAILURE"
 
--- bot/util_dependency_checker.py
+++ bot/util_dependency_checker.py
@@ -4,7 +4,8 @@
 # for CRA-BOT
 
 from github import Github
-
+import urllib2
+import json
 
 '''
 method parameters:
@@ -15,9 +16,60 @@ returns ..
 description..
 
 '''
-def util_dependency_checker(dict_payload):
-	print "dict payload is"
-	pritn dict_payload
+
+PR_COMMENT="issue_comment"
+COMMIT_COMMENT="commit_comment"
+COMMENT_ON_PR="pull"
+PR="pull_request"
+def util_dependency_checker(dict_payload,request_type):
+	url=''
+	print '---------------------------------------------------Request type is ---------------------------------------------------'
+	print request_type
+	#if the comment is made on a PR
+	if(request_type==COMMENT_ON_PR):
+		url=str(dict_payload['issue']['pull_request']['url'])+"/files"
+		response=urllib2.urlopen(url)
+		print "INSIDE COMMENT_ON_PR"
+		data = json.load(response)
+		#print "data is-----------"
+		#print data
+		for item in data:
+			print item['patch']
+			if('xml' in item['filename']):
+				print item['patch']
+
+
+
+	#if the comment is made on a commit
+	elif(request_type==COMMIT_COMMENT):
+		print "INSIDE COMMIT_COMMET"
+		commit_id= dict_payload['comment']['commit_id']
+		path=dict_payload['repository']['commits_url']
+		url=str(path.split('{')[0])+"/"+str(commit_id)
+		print "url is:",url
+		response=urllib2.urlopen(url)
+		data = json.load(response)
+		data=data['files']
+		print "data is ",data
+		for item in data:
+			print item['patch']
+			if('xml' in item['filename']):
+				print item['patch']
+
+
+	#Triggered when a PR is made.
+	elif(request_type==PR):
+		print "INSIDE PR"
+		url=str(dict_payload['pull_request']['url'])+"/files"
+		response=urllib2.urlopen(url)
+		data = json.load(response)
+		for item in data:
+			print item['patch']
+			if('xml' in item['filename']):
+				print item['patch']
+
+
+	print '-------------------End dependency checker---------------------------------------------------'
 	return """
 Dependency Changes:
 ---------------------
