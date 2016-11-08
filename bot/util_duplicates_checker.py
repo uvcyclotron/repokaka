c31c706870a190de558b570059eed7f4bfb0098e
riteshporey <ritesh.porey@gmail.com>
2016-11-08 15:55:25 -0500

    Changed output format

--- bot/util_duplicates_checker.py
+++ bot/util_duplicates_checker.py
@@ -20,9 +20,4 @@ def util_duplicates_checker(currentDirectory):
 	p=subprocess.Popen("/home/ubuntu/pmd/pmd-bin-5.4.1/bin/run.sh cpd --minimum-tokens 10 --files " + currentDirectory , shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	output=p.communicate()[0]
 	return output
-	#return """
-#Found these duplicates:
-#-----------------------
-#[1] ....
-#[2] ....
-#"""
+	return "Found these duplicates:\n-----------------------\n" + str(output)
