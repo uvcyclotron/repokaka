4ac5b4bda3439233a29b475f26f7ddd2ed833be9
riteshporey <ritesh.porey@gmail.com>
2016-11-08 16:02:56 -0500

    Updated Return format of Duplicate Checker

--- bot/util_duplicates_checker.py
+++ bot/util_duplicates_checker.py
@@ -19,5 +19,9 @@ description..
 def util_duplicates_checker(currentDirectory):
 	p=subprocess.Popen("/home/ubuntu/pmd/pmd-bin-5.4.1/bin/run.sh cpd --minimum-tokens 10 --files " + currentDirectory , shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	output=p.communicate()[0]
-	return output
-	return "Found these duplicates:\n-----------------------\n" + str(output)
+	temp = output.strip()
+	if not temp:
+		return "No Duplicates Found" 
+	else:
+		return "Found these duplicates:\n-----------------------\n" + str(output)
+
