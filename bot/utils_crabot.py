dfc4b97543f58863034658387cd6e494a851ceef
dsingh4 <devarshi112@gmail.com>
2016-11-21 16:25:41 -0500

    Refactoring code in clone wrapper

--- bot/utils_crabot.py
+++ bot/utils_crabot.py
@@ -1,11 +1,13 @@
 import urllib2
 import json
+import requests
+
+PR_COMMENT="issue_comment"
+COMMIT_COMMENT="commit_comment"
+COMMENT_ON_PR="pull"
+PR="pull_request"
 
 def get_list_changed_files(dict_payload,request_type):
-	PR_COMMENT="issue_comment"
-	COMMIT_COMMENT="commit_comment"
-	COMMENT_ON_PR="pull"
-	PR="pull_request"
 	#if the comment is made on a PR
 	if(request_type==COMMENT_ON_PR):
 		url=str(dict_payload['issue']['pull_request']['url'])+"/files"
@@ -32,3 +34,33 @@ def get_list_changed_files(dict_payload,request_type):
 		data = json.load(response)
 
 	return data
+
+
+def get_PR_repo_details(dict_payload,request_type):
+	#if the comment is made on a PR
+	if(request_type==COMMENT_ON_PR):
+		pr_url = str(dict_payload['issue']['pull_request']['url'])
+		pull_json_text =  requests.get(pr_url)
+		data = json.loads(pull_json_text.content)
+
+		repouri = data['head']['repo']['clone_url']
+		reponame = data['head']['repo']['name']
+
+	#if the comment is made on a commit
+	elif(request_type==COMMIT_COMMENT):
+		print "INSIDE COMMIT_COMMET"
+		repouri = dict_payload['repository']['clone_url']
+		reponame = dict_payload['repository']['name']
+
+	#Triggered when a PR is made.
+	elif(request_type==PR):
+		print "INSIDE PR"
+		pr_url = str(dict_payload['pull_request']['url'])
+		pull_json_text =  requests.get(pr_url)
+		data = json.loads(pull_json_text.content)
+
+		repouri = data['head']['repo']['clone_url']
+		reponame = data['head']['repo']['name']
+
+
+	return repouri, reponame
