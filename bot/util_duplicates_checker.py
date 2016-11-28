18af4492394a8ed5c4a6e116f18626039c4036ab
riteshporey <ritesh.porey@gmail.com>
2016-11-28 08:47:49 -0500

    Added comments

--- bot/util_duplicates_checker.py
+++ bot/util_duplicates_checker.py
@@ -8,21 +8,21 @@ import subprocess
 
 
 '''
-method parameters:
-	x		is
+method parameters: currentFile is the file to be scanned for duplicates.
 
-returns ..
+returns duplicate code if any. 
 
-description..
+description: pmd is ran over the currentFile to scan for duplicates.
 
 '''
 def util_duplicates_checker(currentFile):
+    # Calling pmd with customized configuration on the currentFile.
 	p=subprocess.Popen("/home/ubuntu/pmd/pmd-bin-5.4.1/bin/run.sh cpd --minimum-tokens 10 --files " + currentFile , shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	output=p.communicate()[0]
 	temp = output.strip()
+	# Returing the duplicates found if any. 
 	if not temp:
 		return "\nNo duplicates found for file: " + currentFile + "\n" 
 	else:
-		#return "\nFound these duplicates:\n-----------------------\n" + str(output)+"\n"
 		return "\nFound these duplicates for file: " + currentFile + "\n" + str(output)+"\n"
 
